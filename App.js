9136e88c6fdd3b53f6d882f14fa40b881ac5c238
Pranjal Rai <pranjalbhs@gmail.com>
2023-01-17 01:49:00 +0530

    implemented add items feature

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [count, increment] = useState(0);
+  const [list, setList] = useState(["mango", "sango", "ango"]);
+  const [currInputVal, setCurrInputVal] = useState("");
+
+  const hanndleAddItem = () => {
+    let ar = [...list];
+    ar.push(currInputVal);
+    setList(ar);
+  };
+  const items = list.map((item, key) => <Text>{item}</Text>);
   return (
     <View style={styles.container}>
-      <Text style={{}}>Todo App</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "center",
+        }}
+      >
+        Todo App
+      </Text>
+      <TextInput
+        placeholder="Add Items"
+        onChangeText={(e) => setCurrInputVal(e)}
+        defaultValue={currInputVal}
+        style={{
+          textAlign: "center",
+          margin: 10,
+          height: 45,
+          borderRadius: 5,
+          borderWidth: 2,
+          borderColor: "grey",
+        }}
+      ></TextInput>
+      <View
+        style={{
+          marginLeft: 15,
+          marginBottom: 5,
+        }}
+      >
+        {items}
+      </View>
+      <View
+        style={{
+          height: 50,
+        }}
+      ></View>
+      <Button
+        onPress={hanndleAddItem}
+        title="Add Item"
+        style={{
+          margin: 5,
+        }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +62,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: 50,
   },
 });
